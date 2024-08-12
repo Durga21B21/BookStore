b3c43f893b90bbe6627ea333e4fe820779c9fc6d
Durga21B21 <nagadurgakolusu@gmail.com>
2024-08-12 07:36:39 +0530

    Frontend!

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import First from "./First/First"; /*home page*/
+import Signup from './components/Signup'
+import Course from "./courses/Courses";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <div className="dark:bg-slate-900 dark:text-white">
+        <Routes>
+          <Route path="/" element={<First />} />
+          <Route path="/course" element={<Course />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/src/First/First.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Banner from '../components/Main'
+import Freebook from '../components/Freebook'
+import Footer from '../components/Footer'
+
+
+function First() {
+  return (
+    <>
+     <Navbar/> 
+     <Banner/>
+     <Freebook/>
+     <Footer/>
+    </>
+  )
+}
+
+export default First;
